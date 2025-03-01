d5ec472b1fc8a16ae14edde2872255408848d611
Gepsi-Quintero <gepsi.quintero@est.iudigital.edu.co>
2025-03-01 13:20:07 -0500

    cargainicial

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pad",
+    version="0.0.1",
+    author="Gepsi Quintero",
+    author_email="gepsi.quintero@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_1"],
+    install_requires=[
+        "pandas",
+        "requests",
+        "matplotlib"
+    ]
+    
+    
+)
